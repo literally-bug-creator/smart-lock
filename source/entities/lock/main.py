e278d0af3481ee46215d5e0b1434a49949b34f21
mrakuse-of <mrakuse.of@gmail.com>
2024-11-26 13:05:58 +0300

    Fix camera entity

--- source/entities/lock/main.py
+++ source/entities/lock/main.py
@@ -6,10 +6,9 @@ class Lock:
         self.__pin = pin
         GPIO.cleanup()
         GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(self.__pin, GPIO.OUT)
 
     def lock(self):
         GPIO.setup(self.__pin, GPIO.IN)
 
     def unlock(self):
-        GPIO.output(self.__pin, GPIO.HIGH)
+        GPIO.setup(self.__pin, GPIO.OUT)
