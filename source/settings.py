4dfaa51dc3abd57695830aae6075dc3565fd5439
literally-bug-creator <literallybugcreator@gmail.com>
2024-11-27 15:25:54 +0300

    Update settings

--- source/settings.py
+++ source/settings.py
@@ -6,7 +6,7 @@ COMMON_ENV = ".env"
 
 
 class CommonSettings(BaseSettings):
-    BACKEND_ENDPOINT_URI: str | None = Field(default=None)
+    BACKEND_ENDPOINT_URI: str | None = Field()
     COOLDOWN: int = Field(default=5)
 
     model_config = SettingsConfigDict(env_file=COMMON_ENV)
