e9238f3b3f6de98bd656df37b8eef361c2e177db
literally-bug-creator <literallybugcreator@gmail.com>
2024-11-29 16:20:48 +0300

    Try to fix issue

--- source/server_api/main.py
+++ source/server_api/main.py
@@ -28,7 +28,9 @@ class ServerAPI:
     def _request_identify(self, frame_path):
         with open(frame_path, "rb") as frame:
             request = requests.post(
-                self.__settings.URL, files={"file": ("frame.png", frame, "image/png")}
+                self.__settings.URL,
+                files={"file": ("frame.png", frame, "image/png")},
+                verify=False,
             )
 
             return request.status_code == 200
