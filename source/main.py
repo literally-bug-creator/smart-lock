ddd8db658d26abfe51270499e85efa281365321c
literally-bug-creator <literallybugcreator@gmail.com>
2024-11-29 15:01:41 +0300

    Remove .idea Update main Rename frame-processor to frame_processor

--- source/main.py
+++ source/main.py
@@ -1,1 +1,7 @@
-from camera import get_camera
+from camera import get_camera, camera_exceptions
+from frame_processor import get_frame_processor, frame_processor_exceptions
+
+
+if __name__ == "__main__":
+    camera = get_camera()
+    frame_processor = get_frame_processor()
