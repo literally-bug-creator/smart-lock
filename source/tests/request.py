b88ac8f1f4da507bb0fbe2658e0fe5cf73329708
literally-bug-creator <literallybugcreator@gmail.com>
2024-11-29 16:33:22 +0300

    Add image

--- source/tests/request.py
+++ source/tests/request.py
@@ -1,6 +1,6 @@
 import requests
 
-with open("a.png", "rb") as image:
+with open("png.png", "rb") as image:
     request = requests.post(
         "http://localhost:8000/identify",
         files={"file": ("image.png", image, "image/png")},
