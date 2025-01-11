83df2c37a512421ed09a2564598bb0f364b317f2
literally-bug-creator <literallybugcreator@gmail.com>
2025-01-11 21:26:43 +0300

    Remove face detection on client logic

--- source/main.py
+++ source/main.py
@@ -1,9 +1,10 @@
 from camera import get_camera, camera_exceptions
 from lock import get_lock, lock_exceptions
-from frame_processor import get_frame_processor, frame_processor_exceptions
 from server_api import get_server_api, server_api_exceptions
 from time import sleep
 from logs import logger
+import os
+import cv2
 
 
 def main():
@@ -13,11 +14,12 @@ def main():
         logger.debug("Get 'None' frame!")
         return
 
-    if not frame_processor.contains_face(frame):
-        logger.debug("Frame does not contain faces!")
-        return
+    is_saved = cv2.imwrite("frame.png", frame)
 
-    frame_path = frame_processor.save_frame(frame)
+    if not is_saved:
+        raise Exception("Can't save the frame!")
+    
+    frame_path = os.path.abspath(os.path.join(os.getcwd(), "frame.png"))
     is_identified = server_api.request_identify(frame_path)
 
     if not is_identified:
@@ -28,7 +30,9 @@ def main():
 
     lock.unlock()
     logger.debug("Lock is opened!")
+
     sleep(3)
+    
     lock.lock()
     logger.debug("Lock is closed!")
 
@@ -36,7 +40,6 @@ def main():
 if __name__ == "__main__":
     camera = get_camera()
     lock = get_lock()
-    frame_processor = get_frame_processor()
     server_api = get_server_api()
 
     while True:
@@ -50,7 +53,6 @@ if __name__ == "__main__":
                     server_api_exceptions.ServerAPIError,
                     camera_exceptions.CameraException,
                     lock_exceptions.LockException,
-                    frame_processor_exceptions.FrameProcessorError,
                 ),
             ):
                 logger.critical(str(e))
